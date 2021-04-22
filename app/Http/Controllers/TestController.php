4b042bfb43f340a07ced64218e3e60c955e16833
Segun Akanbi <akanbisegun46@yahoo.com>
2021-04-22 16:53:19 +0100

    Added conversion of xml to json to get uuid

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -48,10 +48,10 @@ class TestController extends Controller
     {
         //$response = Http::post('https://my346325.sapbydesign.com/sap/bc/srt/scs/sap/managejournalentryin');
         //dd($response);
-        
+
         $url = 'https://my346325.sapbydesign.com/sap/bc/srt/scs/sap/managejournalentryin';
 
-        $xml = 
+        $xml =
         '
         <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:glob="http://sap.com/xi/SAPGlobal20/Global">
    <soap:Header/>
@@ -60,7 +60,7 @@ class TestController extends Controller
             <BasicMessageHeader>
             <ID>ABCDEF000000001</ID>
             </BasicMessageHeader>
-            <JournalEntry>   
+            <JournalEntry>
                 <ObjectNodeSenderTechnicalID>1</ObjectNodeSenderTechnicalID>
                 <CompanyID>2000</CompanyID>
                 <OriginalEntryDocumentReferenceID>00047</OriginalEntryDocumentReferenceID>
@@ -105,7 +105,7 @@ class TestController extends Controller
             dd('FAILED!'.$response->status());
         }
 
-        
+
 
     }
 
@@ -116,10 +116,10 @@ class TestController extends Controller
 
 
     public function test2()
-    { 
+    {
         $url = 'https://my346325.sapbydesign.com/sap/bc/srt/scs/sap/managejournalentryin';
 
-        $xml = 
+        $xml =
         '
         <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:glob="http://sap.com/xi/SAPGlobal20/Global">
    <soap:Header/>
@@ -128,7 +128,7 @@ class TestController extends Controller
             <BasicMessageHeader>
             <ID>ABCDEF000000001</ID>
             </BasicMessageHeader>
-            <JournalEntry>   
+            <JournalEntry>
                 <ObjectNodeSenderTechnicalID>1</ObjectNodeSenderTechnicalID>
                 <CompanyID>2000</CompanyID>
                 <OriginalEntryDocumentReferenceID>00047</OriginalEntryDocumentReferenceID>
@@ -183,8 +183,41 @@ class TestController extends Controller
         }
     }
 
+    function removeNamespaceFromXML( $xml )
+    {
+        // Because I know all of the the namespaces that will possibly appear in
+        // in the XML string I can just hard code them and check for
+        // them to remove them
+        $toRemove = ['soap', 'n0', 'soap-env'];
+        // This is part of a regex I will use to remove the namespace declaration from string
+        $nameSpaceDefRegEx = '(\S+)=["\']?((?:.(?!["\']?\s+(?:\S+)=|[>"\']))+.)["\']?';
+
+        // Cycle through each namespace and remove it from the XML string
+        foreach( $toRemove as $remove ) {
+            // First remove the namespace from the opening of the tag
+            $xml = str_replace('<' . $remove . ':', '<', $xml);
+            // Now remove the namespace from the closing of the tag
+            $xml = str_replace('</' . $remove . ':', '</', $xml);
+            // This XML uses the name space with CommentText, so remove that too
+            $xml = str_replace($remove . ':commentText', 'commentText', $xml);
+            // Complete the pattern for RegEx to remove this namespace declaration
+            $pattern = "/xmlns:{$remove}{$nameSpaceDefRegEx}/";
+            // Remove the actual namespace declaration using the Pattern
+            $xml = preg_replace($pattern, '', $xml, 1);
+        }
+
+        // Return sanitized and cleaned up XML with no namespaces
+        return $xml;
+    }
+
+    function namespacedXMLToArray($xml)
+    {
+        // One function to both clean the XML string and return an array
+        return json_decode(json_encode(simplexml_load_string($this->removeNamespaceFromXML($xml))), true);
+    }
+
     public function test3()
-    { 
+    {
 
         // POST API URL
         $url = "https://my346325.sapbydesign.com/sap/bc/srt/scs/sap/managejournalentryin";
@@ -216,7 +249,7 @@ class TestController extends Controller
         $ObjectNodeSenderTechnicalID3 = '3'; //item Debit Section
         $OriginalEntryDocumentItemReferenceItemID1 = '1'; //item Credit Section
         $OriginalEntryDocumentItemReferenceItemID2 = '1'; //item Debit Section
-        $GeneralLedgerAccountAliasCode = 'A-7140'; 
+        $GeneralLedgerAccountAliasCode = 'A-7140';
         $BusinessTransactionCurrencyAmount = 555;
         $currencyCode = 'NGN';
 
@@ -234,7 +267,7 @@ class TestController extends Controller
                     <ID>$ID</ID>
                     </BasicMessageHeader>
 
-                    <JournalEntry>   
+                    <JournalEntry>
                         <ObjectNodeSenderTechnicalID>$ObjectNodeSenderTechnicalID1</ObjectNodeSenderTechnicalID>
                         <CompanyID>$CompanyID</CompanyID>
                         <OriginalEntryDocumentReferenceID>$OriginalEntryDocumentReferenceID</OriginalEntryDocumentReferenceID>
@@ -263,7 +296,7 @@ class TestController extends Controller
 
         </soap:Envelope>
         DATA;
-        
+
 
     //HTTP POST REQUEST
     try {
@@ -274,21 +307,35 @@ class TestController extends Controller
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_xml);
-        
+
         //for debug only!
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        
+
         $response = curl_exec($curl);
         $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
         $xml=simplexml_load_string($data_xml);
         $message = $xml->CompanyID;
-        
-        dd($status.' : '.$message.' : '.$response);
+
+        $json_converted = $this->namespacedXMLToArray($response);
+        // dd($json_converted);
+        if(array_key_exists('JournalEntryBundleCreateConfirmation', $json_converted['Body']))
+        {
+            if(array_key_exists('JournalEntryCreateConfirmationBundleJournalEntry', $json_converted['Body']['JournalEntryBundleCreateConfirmation']))
+            {
+                dd($json_converted['Body']['JournalEntryBundleCreateConfirmation']['JournalEntryCreateConfirmationBundleJournalEntry']['UUID']);
+            }else {
+                dd($json_converted['Body']['JournalEntryBundleCreateConfirmation']);
+            }
+        }else if(array_key_exists('Fault', $json_converted['Body'])) {
+            dd($json_converted['Body']['Fault']);
+        }
+        // dd($json_converted['Body']['JournalEntryBundleCreateConfirmation']['JournalEntryCreateConfirmationBundleJournalEntry']['UUID']);
+        // dd($status.' : '.$message.' : '.$response);
 
         curl_close($curl);
-        } 
+        }
     catch (Exception $e) {
                 //echo 'and the error is: ',  $e->getMessage(), "\n";
                 //\Session::flash('failed', 'A Connection Error has occured! Check that your API URL and Parameters are correct.');
@@ -311,17 +358,17 @@ class TestController extends Controller
 
 
 
-        
+
     }
 
 
 
 
     public function test4()
-    { 
+    {
 
         $curl = curl_init();
-        
+
         curl_setopt_array($curl, array(
           CURLOPT_URL => 'https://my346325.sapbydesign.com/sap/bc/srt/scs/sap/managejournalentryin',
           CURLOPT_RETURNTRANSFER => true,
@@ -338,7 +385,7 @@ class TestController extends Controller
                     <BasicMessageHeader>
                     <ID>ABCDEF000000001</ID>
                     </BasicMessageHeader>
-                    <JournalEntry>   
+                    <JournalEntry>
                         <ObjectNodeSenderTechnicalID>1</ObjectNodeSenderTechnicalID>
                         <CompanyID>2000</CompanyID>
                         <OriginalEntryDocumentReferenceID>00047</OriginalEntryDocumentReferenceID>
@@ -368,7 +415,7 @@ class TestController extends Controller
             'Cookie: sap-usercontext=sap-client=161'
           ),
         ));
-        
+
         $response = curl_exec($curl);
 
        //$status = $response->getStatusCode('staus');
@@ -379,12 +426,12 @@ class TestController extends Controller
 
         curl_close($curl);
 
-        
+
     }
 
 
     public function test5()
-    { 
+    {
         $soapXMLResult=<<<XML
             <soap-env:Envelope xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/">
                 <soap-env:Header/>
@@ -397,7 +444,7 @@ class TestController extends Controller
                 </soap-env:Body>
             </soap-env:Envelope>
         XML;
-        
+
         $soap     = simplexml_load_string($soapXMLResult);
         $response = $soap->children('http://schemas.xmlsoap.org/soap/envelope/')->Body->children()->Fault;
         $customerId = (string) $response->faultcode;
@@ -412,25 +459,25 @@ class TestController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    { 
+    {
         //create form
         $pid_user = Auth::user()->pid_user;
         $data = array();
 
         $sap_communication_arrangements = DB::table('sap_communication_arrangements')
             ->where('pid_user', '=', $pid_user)
-            ->get(); 
+            ->get();
 
         $sap_financial_services = DB::table('sap_financial_services')
             ->where('pid_user', '=', $pid_user)
-            ->get(); 
+            ->get();
 
         //DATA FOR VIEW
         $data['sap_financial_services'] = $sap_financial_services;
 
         $data['company_name'] = Auth::user()->company_name;
         $data['sap_communication_arrangements'] = $sap_communication_arrangements;
-        
+
 
         //\Session::flash('success', 'WebHR-Configuration was successfully created!');
         return view('pages/sap_communication_arrangements_create', $data);
@@ -447,7 +494,7 @@ class TestController extends Controller
     public function store(Request $request)
     {
 
-        //META DATA 
+        //META DATA
         $data = array();
         $pid_user = Auth::user()->pid_user;
 
@@ -465,7 +512,7 @@ class TestController extends Controller
                 'ca_description' => $request->ca_description,
                 'ca_username' => $request->ca_username,
                 'ca_password' =>  $request->ca_password,
-                'ca_status1' =>  $request->ca_status1, 
+                'ca_status1' =>  $request->ca_status1,
                 'ca_status2' =>  $request->ca_status2,
                 'ca_ext1' => 'NA',
                 'ca_ext2' => 'NA',
@@ -477,7 +524,7 @@ class TestController extends Controller
 
             $sap_communication_arrangements = DB::table('sap_communication_arrangements')
                 ->where('pid_user', '=', $pid_user)
-                ->get(); 
+                ->get();
 
             //DATA FOR VIEW
             $data['sap_communication_arrangements'] = $sap_communication_arrangements;
@@ -497,7 +544,7 @@ class TestController extends Controller
      */
     public function show($sap_communication_arrangements)
     {
-        //META DATA 
+        //META DATA
         $data = array();
         $pid_user = Auth::user()->pid_user;
 
@@ -505,7 +552,7 @@ class TestController extends Controller
         $sap_communication_arrangements = DB::table('sap_communication_arrangements')
             ->where('pid_user', '=', $pid_user)
             ->where('pid_ca', '=', $sap_communication_arrangements)
-            ->get(); 
+            ->get();
 
         //DATA FOR VIEW
         $data['company_name'] = Auth::user()->company_name;
@@ -525,7 +572,7 @@ class TestController extends Controller
      */
     public function edit($sap_communication_arrangements)
     {
-        //META DATA 
+        //META DATA
         $data = array();
         $pid_user = Auth::user()->pid_user;
 
@@ -535,11 +582,11 @@ class TestController extends Controller
             ->select('*','sap_financial_services.fsrv_name as fsrv_namex')
             ->where('sap_communication_arrangements.pid_user', '=', $pid_user)
             ->where('sap_communication_arrangements.pid_ca', '=', $sap_communication_arrangements)
-            ->get(); 
+            ->get();
 
         $sap_financial_services = DB::table('sap_financial_services')
             ->where('pid_user', '=', $pid_user)
-            ->get(); 
+            ->get();
 
 
         //DATA FOR VIEW
@@ -563,7 +610,7 @@ class TestController extends Controller
     public function update(Request $request, $sap_communication_arrangements)
     {
 
-        //META DATA 
+        //META DATA
         $data = array();
         $pid_user = Auth::user()->pid_user;
 
@@ -587,7 +634,7 @@ class TestController extends Controller
             ->join('sap_financial_services', 'sap_financial_services.pid_fsrv', '=', 'sap_communication_arrangements.pid_fsrv')
             ->select('*','sap_financial_services.fsrv_name as fsrv_namex')
             ->where('sap_communication_arrangements.pid_user', '=', $pid_user)
-            ->get(); 
+            ->get();
 
         //DATA FOR VIEW
         $data['company_name'] = Auth::user()->company_name;
@@ -599,7 +646,7 @@ class TestController extends Controller
 
 
 
-    
+
     /**
      * Remove the specified resource from storage.
      *
@@ -608,7 +655,7 @@ class TestController extends Controller
      */
     public function destroy($sap_communication_arrangements)
     {
-        //META DATA 
+        //META DATA
         $data = array();
         $pid_user = Auth::user()->pid_user;
 
@@ -623,7 +670,7 @@ class TestController extends Controller
             ->join('sap_financial_services', 'sap_financial_services.pid_fsrv', '=', 'sap_communication_arrangements.pid_fsrv')
             ->select('*','sap_financial_services.fsrv_name as fsrv_namex')
             ->where('sap_communication_arrangements.pid_user', '=', $pid_user)
-            ->get(); 
+            ->get();
 
         //DATA FOR VIEW
         $data['company_name'] = Auth::user()->company_name;
@@ -637,53 +684,53 @@ class TestController extends Controller
         //TEST WEBHR PAYROLL CONFIG
         public function payroll_config_test(Request $request)
         {
-            //META DATA 
+            //META DATA
             $data = array();
             $pid_user = Auth::user()->pid_user;
-    
+
             //config
             $pid_config = $request->pid_config;
-     
+
             //config details
             $get_url = DB::table('webhr_payroll_config')
                 ->where('pid_user', $pid_user)
                 ->where('pid_config', $pid_config)
                 ->value('get_url');
-    
+
             $authorization_code = DB::table('webhr_payroll_config')
                 ->where('pid_user', $pid_user)
                 ->where('pid_config', $pid_config)
                 ->value('authorization_code');
-    
-    
+
+
             //HTTP GET REQUEST
             $token = $authorization_code;
             $url = $get_url;
             $pre_param = '?module=Payroll&submodule=Payslips&request=List&';
             $params = 'params={"PayslipStartMonth": "01", "PayslipStartYear": "2021", "PayslipEndMonth": "12","PayslipEndYear": "2021", "Company": "AGPC", "Division": "", "Station": "Lagos"}';
-    
+
             //create form
             $data = array();
             $data['company_name'] = Auth::user()->company_name;
-    
+
             $webhr_payroll_config = DB::table('webhr_payroll_config')
                 ->where('pid_user', '=', $pid_user)
-                ->get(); 
-    
+                ->get();
+
             $data['company_name'] = Auth::user()->company_name;
             $data['webhr_payroll_config'] = $webhr_payroll_config;
-    
+
                     //HTTP GET RESPONSE
                     try {
                         $response = Http::withToken($token)->get($url.$pre_param.$params);
-                    } 
+                    }
                 catch (Exception $e) {
                         //echo 'and the error is: ',  $e->getMessage(), "\n";
                         \Session::flash('failed', 'A Connection Error has occured! Check that your API URL and Parameters are correct.');
                         return view('pages/webhr_payroll_config_view', $data);
                         exit;
                     }
-    
+
                     //RESPONSE
                     if($response->status() == 200){
                         \Session::flash('success', 'Connection SUCCESSFUL! Status: 200 : OK WebHR-Configuration Test was Successful.');
@@ -692,7 +739,7 @@ class TestController extends Controller
                         \Session::flash('failed', 'Connection FAILED! Connection to WEB-HR :: WebHR-Configuration Test Failed.');
                         $data['status'] = 400;
                     }
-    
+
             //\Session::flash('success', 'WebHR-Configuration was successfully created!');
             return view('pages/webhr_payroll_config_view', $data);
         }
